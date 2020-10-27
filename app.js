2f51c2d563cd5d0f2366a3a8e4c0abf3b1d10e0d
rai8 <hytonne@outlook.com>
2020-10-27 07:31:13 +0300

    using the template engine

--- app.js
+++ app.js
@@ -6,20 +6,40 @@ const PORT = process.env.PORT || 3000
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
   //res.send(`<p>Home Page</p>`)
-  res.sendFile('./views/index.html', { root: __dirname })
+  //res.sendFile('./views/index.html', { root: __dirname })
+  const blogs = [
+    /*  { title: 'Yoshi finds egs', snippet: 'Lorem ipsum sit amet consectetur' },
+    { title: 'Mario finds stars', snippet: 'Lorem ipsum sit amet consectetur' },
+    { title: 'How to defeat browser', snippet: 'Lorem ipsum sit amet consectetur' }, */
+  ]
+  res.render('index', {
+    title: 'Home',
+    blogs,
+  })
 })
 
 app.get('/about', (req, res) => {
-  res.sendFile('./views/about.html', { root: __dirname })
+  // res.sendFile('./views/about.html', { root: __dirname })
+  res.render('about', {
+    title: 'About',
+  })
 })
 //handling redirection
 app.get('/about-me', (req, res) => {
-  res.redirect('/about')
+  //res.redirect('/about')
+  res.render('about')
+})
+app.get('/blogs/create', (req, res) => {
+  res.render('create', {
+    title: 'Create',
+  })
 })
 
 //handling 404 pages
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname })
+  res.status(404).render('404', {
+    title: 'Not found',
+  })
 })
 // listening to server
 app.listen(PORT, () => {
