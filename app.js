6d00bf6397c2cf111b8a7300c70dab1630569eb0
rai8 <hytonne@outlook.com>
2020-10-27 11:53:26 +0300

    serving static files

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 const PORT = process.env.PORT || 3000
 
 //seeting up to use ejs
 app.set('view engine', 'ejs')
+
+//serving static files- css , images
+app.use(express.static('public'))
+//logger middleware
+app.use(morgan('dev'))
+/* app.use((req, res, next) => {
+  console.log('new request')
+  console.log('host', req.hostname)
+  console.log('path', req.path)
+  console.log('method', req.method)
+  next()
+}) */
+
 app.get('/', (req, res) => {
   //res.send(`<p>Home Page</p>`)
   //res.sendFile('./views/index.html', { root: __dirname })
@@ -25,10 +39,10 @@ app.get('/about', (req, res) => {
   })
 })
 //handling redirection
-app.get('/about-me', (req, res) => {
+/* app.get('/about-me', (req, res) => {
   //res.redirect('/about')
   res.render('about')
-})
+}) */
 app.get('/blogs/create', (req, res) => {
   res.render('create', {
     title: 'Create',
